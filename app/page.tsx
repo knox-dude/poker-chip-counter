adbb72a7b2d9fefa21d07c2978797ab74b1d7cdb
Andrew Knox <andrew.knox.r@gmail.com>
2024-08-29 17:53:05 -0700

    adding refs for image and fileInput

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-"use client"
+"use client";
 import VisionImage from "./components/VisionImage";
-import UploadButtons from "./components/UploadButtons";
-import React, { useState } from "react";
+import Button from "./components/Button";
+import React, { useState, useRef, useEffect } from "react";
 
 export default function Home() {
-
-  const DEFAULTIMAGE = "/defaultImage.png"
+  const DEFAULTIMAGE = "/defaultImage.png";
   const [selectedImage, setSelectedImage] = useState<string>(DEFAULTIMAGE);
+  const [imageWidth, setImageWidth] = useState<number>(0);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const imageRef = useRef<HTMLImageElement | null>(null);
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -19,11 +21,38 @@ export default function Home() {
     }
   };
 
+  const handleUploadClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  useEffect(() => {
+    if (imageRef.current) {
+      setImageWidth(imageRef.current.clientWidth);
+    }
+  }, [selectedImage]);
+
   return (
     <main className="flex min-h-screen flex-col items-center gap-4 justify-center p-24">
       <h1 className="text-3xl font-bold text-center">Poker Chip Counter</h1>
-      <VisionImage imageUrl={DEFAULTIMAGE} altText="Default Image" />
-      <UploadButtons />
+      <VisionImage
+        imageUrl={selectedImage}
+        altText="Uploaded Image"
+        ref={imageRef}
+      />
+      <div
+        className="flex justify-around"
+        style={{ width: `${imageWidth}px` }}
+      >
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageUpload}
+          ref={fileInputRef}
+          style={{ display: "none" }}
+        />
+        <Button text="Upload Image" action={handleUploadClick} />
+        <Button text="Analyze Chips" action={() => console.log("Analyze Chips")} />
+      </div>
     </main>
   );
 }
