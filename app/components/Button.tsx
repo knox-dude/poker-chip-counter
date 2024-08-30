5d8ad18d4170c77e265d277aaf08a4647832bf56
Andrew Knox <andrew.knox.r@gmail.com>
2024-08-29 17:53:19 -0700

    removing some unnecessary stuff

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 interface ButtonProps {
   text: string;
@@ -6,15 +6,14 @@ interface ButtonProps {
 }
 
 const Button = ({ text, action }: ButtonProps) => {
-
   return (
-      <button
-        type="submit"
-        className="bg-slate-400 mt-4 mb-4 text-white rounded-md p-2 disabled:cursor-not-allowed disabled:opacity-30 enabled:hover:bg-paper-600"
-        onSubmit={() => action()}
-      >
-        {text}
-      </button>
+    <button
+      type="button"
+      className="bg-slate-400 mt-4 mb-4 text-white rounded-md p-2 disabled:cursor-not-allowed disabled:opacity-30 enabled:hover:bg-paper-600"
+      onClick={action}
+    >
+      {text}
+    </button>
   );
 };
 
